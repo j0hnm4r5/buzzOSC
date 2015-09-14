86b2d68f3bfd55809320302f04eb3f8e06a74fc5
John Mars <JOHN@M4R5.io>
2015-09-13 22:44:05 -0400

    Initial Commit

---
+++ buzzOSC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buzzOSC
+//
+//  Created by John Mars on 9/13/15.
+//  Copyright © 2015 M4R5. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
